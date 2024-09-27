0f9b2affaf926bece5102349fe208f0cea7dc709
Camilo-romero-cruz <romerocruzcesarcamilo@gmail.com>
2024-09-27 11:11:24 -0500

    se ajusta nav bar para que funcione la busqueda y lo guarde en un local store

--- assets/js/Vista_Principal.js
+++ assets/js/Vista_Principal.js
@@ -1,35 +1,39 @@
+// Definición de los colores asociados a los tipos de Pokémon
 const colores = {
-    normal: '#F5F5F5', 
+    normal: '#F5F5F5',
     fighting: '#F28C6F',
-    flying: '#D5A6BD', 
-    poison: '#D6A1B1', 
-    ground: '#F7B7A3', 
-    rock: '#D5B8A1', 
-    bug: '#C2D6A1', 
+    flying: '#D5A6BD',
+    poison: '#D6A1B1',
+    ground: '#F7B7A3',
+    rock: '#D5B8A1',
+    bug: '#C2D6A1',
     ghost: '#D0A8D3',
-    steel: '#D0D0D0', 
-    fire: '#F5A97B', 
+    steel: '#D0D0D0',
+    fire: '#F5A97B',
     water: '#A3C8E4',
-    grass: '#A8D5A2', 
+    grass: '#A8D5A2',
     electric: '#F9E58F',
-    psychic: '#F8BBD0', 
-    ice: '#A2D8E0', 
-    dragon: '#C8A2D8', 
-    dark: '#6E6E6E', 
-    fairy: '#F6C1C0', 
+    psychic: '#F8BBD0',
+    ice: '#A2D8E0',
+    dragon: '#C8A2D8',
+    dark: '#6E6E6E',
+    fairy: '#F6C1C0',
     unknown: '#FFEBE8',
-    shadow: '#3A4D6E' 
+    shadow: '#3A4D6E'
 };
 
+// Obtener los tipos de Pokémon
 const tipoPokemones = Object.keys(colores);
 const contenedor = document.getElementById('poke-container');
 let numeroPokemones = 0; // Controlar el número de Pokémon cargados
-const limite = 20; // Número de Pokémon a cargar por vez
+const limite = 25; // Número de Pokémon a cargar por vezç
+
+const start = 25;
 
 // Función para obtener Pokémon desde la API
 const obtenerPokemones = async (start, limit) => {
     const promises = [];
-    for (let i = start; i < start + limit; i++) {
+    for (let i = 1; i < start + limit; i++) {
         promises.push(traerPokemon(i));
     }
     await Promise.all(promises);
@@ -53,10 +57,9 @@ const crearTarjetaPokemon = (pokemon) => {
     const pokemonEl = document.createElement('div');
     pokemonEl.classList.add('pokemon');
 
-    const nombre = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+    const nombre = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
     const id = pokemon.id.toString().padStart(3, '0');
     const types = pokemon.types.map(type => type.type.name); // Obtener todos los tipos
-    const color = colores[types[0]] || '#eee'; // Usar el color del primer tipo
 
     // Crear HTML para mostrar todos los tipos como píldoras
     const tiposHTML = types.map(type => `
@@ -65,7 +68,7 @@ const crearTarjetaPokemon = (pokemon) => {
 
     const pokemonInnerHTML = `
     <a href="/pokemon.html" class="tarjeta link-dark link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
-        <div class="img-container" style="background-color: ${color};"> <!-- Asignar color aquí -->
+        <div class="img-container"> <!-- Sin asignar color aquí -->
             <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png" alt="${pokemon.name}" style="width: 65%;">
         </div>
         <div class="info">
@@ -79,7 +82,6 @@ const crearTarjetaPokemon = (pokemon) => {
     pokemonEl.innerHTML = pokemonInnerHTML; // Asignar HTML al contenedor principal
     contenedor.appendChild(pokemonEl);
 };
-
 
 // Función para manejar el scroll y cargar más Pokémon
 const manejarScroll = () => {
@@ -97,3 +99,6 @@ const init = () => {
 
 // Iniciar la aplicación
 init();
+
+
+
